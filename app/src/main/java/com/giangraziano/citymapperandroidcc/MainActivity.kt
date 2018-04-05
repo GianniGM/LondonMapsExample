9d6b7c535190e76194fb1083c6af60e9e0ef8861
GianniGM <giangraziano@gmail.com>
2018-04-05 23:34:51 +0200

    added order by received data

--- app/src/main/java/com/giangraziano/citymapperandroidcc/MainActivity.kt
+++ app/src/main/java/com/giangraziano/citymapperandroidcc/MainActivity.kt
@@ -87,6 +87,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun parseReceivedRawData(it: List<Arrival>) {
         val parsedData = it
+                .sortedBy { it.timeToStation }
                 .groupBy { it.naptanId }
                 .map {
                     StationInfo(
